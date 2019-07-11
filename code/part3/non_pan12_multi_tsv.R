b4dfbff116a03027b2ddc64222beb3554ab69924
ATLi2001 <42742696+ATLi2001@users.noreply.github.com>
2019-07-11 16:42:00 -0400

    Add files via upload

---
+++ code/part3/non_pan12_multi_tsv.R
@@ -0,0 +1,44 @@
+#list of patients with successful parsing
+patient_list <- read.table("all_phylowgs.txt",stringsAsFactors = FALSE,header = FALSE)
+patient_list <- patient_list$V1
+
+#leave only TCGA Barcode
+patient_list <- substring(patient_list,16,31)
+
+#cut down to only non_pan12
+non_pan12 <- read.table("~/Desktop//Austin/MSTC/Capstone Project/Austin Pipeline MC3/TitanCNA/non_pan12_jobs_list.txt",
+                        stringsAsFactors = FALSE,header = FALSE)
+non_pan12 <- non_pan12$V1
+patient_list <- intersect(patient_list,non_pan12)
+
+#create a blank dataframe for the tsv
+tsv <- as.data.frame(matrix("", nrow = 0, ncol = 9), stringsAsFactors = FALSE)
+#column names for tsv
+colnames(tsv) <- c("--input SSM", "--input CNV", "--output-recursive OUTPUT_DIR",
+                   "n","r","I","B","s","i")
+
+#fill out the tsv
+for(i in 1:length(patient_list)){
+  #--input SSM
+  tsv_input <- paste("gs://austin_pipeline_mc3/phylowgs/non_pan12/INPUT/phylowgs_input",patient_list[i],
+                     "ssm_data.txt", sep = "/")
+  #--input CNA
+  tsv_cna <- paste("gs://austin_pipeline_mc3/phylowgs/non_pan12/INPUT/phylowgs_input", patient_list[i],
+                   "cnv_data.txt", sep = "/")
+  #--output OUTPUT_DIR
+  tsv_output <- paste("gs://austin_pipeline_mc3/phylowgs/non_pan12/OUTPUT",patient_list[i],
+                      sep = "/")
+  #note that default n=4,B=1000,s=2500,i=5000
+  #r=-1 means let phylowgs choose a random seed
+  #I=inf means keep all trees
+  tsv_add <- c(tsv_input, tsv_cna, tsv_output,4,-1,"inf",1000,2500,5000)
+  #make into a dataframe to then attach to the main output
+  tsv_add <- as.data.frame(tsv_add)
+  tsv_add <- t(tsv_add)
+  colnames(tsv_add) <- c("--input SSM", "--input CNV", "--output-recursive OUTPUT_DIR",
+                         "n","r","I","B","s","i")
+  
+  tsv <- rbind.data.frame(tsv, tsv_add, stringsAsFactors = FALSE)
+}
+
+write.table(tsv, file="non_pan12_multi.tsv",sep="\t",row.names=F,col.names = T,quote=F)
